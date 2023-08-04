b283d48aea4c06573e6790b705d75f389eb7a561
Trevor-crypto243 <otitrevor450@gmail.com>
2023-08-04 16:59:31 +0300

    Computed properties

--- app.js
+++ app.js
@@ -7,12 +7,25 @@ const app = Vue.createApp({
         return{
             title:"The final empire",
             age:1,
-            show:'false'
+            show:'false',
+            books:[
+                {title:'Asiko', author: 'Trevor',img:'assets/1.png',isFav: true},
+                {title:'Ashley', author: 'Ogina',img:'assets/2.png',isFav: false},
+                {title:'Achieng', author: 'Lennox ',img:'assets/3.png',isFav: true},
+            ]
         }
     },
     methods:{
         changeTitle(her){
             this.title = her
+        },
+        toggleFav(book){
+          book.isFav = !book.isFav   
+        }
+    },
+    computed:{
+        filteredBooks(){
+            return  this.books.filter((book)=>book.isFav)
         }
     }
 }) //creates vue app
